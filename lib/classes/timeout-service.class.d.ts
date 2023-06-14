19a767ee58b5b1b582b0b33c5f8097568a661166
Younes Lahouiti <loller763@gmail.com>
2023-06-14 14:20:16 +0200

    Compiled the TS file + updated the SemVer

--- lib/classes/timeout-service.class.d.ts
+++ lib/classes/timeout-service.class.d.ts
@@ -1,32 +1,32 @@
 /// <reference types="node" />
 /**
- * Utility class that sets and clears intervals
+ * Utility class that sets and clears timeoutServices
  */
-export declare class IntervalService {
+export declare class TimeoutService {
     static id: NodeJS.Timer;
     static arrayOfIds: NodeJS.Timer[];
     /**
-     * Method that creates an interval
+     * Method that creates a timeoutService
      *
      * @param milliseconds Duration of the timer in milliseconds before executing the callback function
      * @param {(...args: any) => any | void} callback Callback function that will be called after the timer runs out
      * @param {...functionArguments} functionArguments Arguments for the callback function
      *
-     * @returns A number as an ID for the interval
+     * @returns A number as an ID for the timeoutService
      *
      * @example
      * let fct = (text) => {
      *   console.log(text);
      * };
      *
-     * let intervalTrigger = Interval.set(2_500, fct, "Hello world!");
+     * let timeoutServiceTrigger = TimeoutService.set(2_500, fct, "Hello world!");
      *
      */
     static set(milliseconds: number, callback: (...functionArguments: any) => any, ...functionArguments: any[]): NodeJS.Timer;
     /**
-     * Method that clears an interval
+     * Method that clears an timeoutService
      *
-     * @param {number} id ID of the interval to clear (stored inside the trigger of the interval)
+     * @param {number} id ID of the timeoutService to clear (stored inside the trigger of the timeoutService)
      *
      * @example
      *
@@ -34,11 +34,11 @@ export declare class IntervalService {
      *   console.log(text);
      * };
      *
-     * let intervalTrigger = Interval.set(2_500, fct, "Hello world!");
+     * let timeoutServiceTrigger = TimeoutService.set(2_500, fct, "Hello world!");
      *
      * // ...
      *
-     * Interval.clear(intervalTrigger);
+     * TimeoutService.clear(timeoutServiceTrigger);
      *
      */
     static clear(id: NodeJS.Timer): void;
